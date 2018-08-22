6fc2f62d7cc6cb9d57173238a4e979d8403d1548
Mike Nguyen <nguyenduchung0609@gmail.com>
2018-08-22 16:02:31 +0800

    add singleton
    static read only and private construtor

---
+++ DesignPatter.Code.Sample.Dotnet/Creation/Singleton/Singleton.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatter.Code.Sample.Dotnet.Creation.Singleton
+{
+    public sealed class Singleton
+    {
+        private static readonly Singleton instance = new Singleton();
+
+        private Singleton()
+        {
+            
+        }
+
+        public string Message = "";
+
+        public static Singleton Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }     
+    }
+}
